b5e74ee0c38b3a8f628427e630a1f518440ed157
ahmad786-code <ahmadrazadev2@gmail.com>
2024-07-25 14:32:37 +0500

    desing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,9 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      <header className="header">
-        <nav className="navbar">
-          <div className="logos">
-            <a className="logo-text" href="#">PixieMGMT</a>
-          </div>
+      <header className="header relative">
 
+        <nav className="navbar">
           <div className="img-logo">
             <img src="../../logo.png" alt="logo" className="logo-img" />
           </div>
@@ -18,80 +15,83 @@ export default function Home() {
             <li>
               <a className="social-btn" href="#">Social</a>
             </li>
-            <li>
-              <a className="contact-btn" href="#contact-section">Contact</a>
-            </li>
 
           </ul>
-          <div className="toggle-btn">
-            <button type="button" className="nav-toggle" id="nav-toggle">
-              <i className="fas fa-bars"></i>
-            </button>
-          </div>
-        </nav>
 
+        </nav>
 
         <div className="text-box">
           <h1 className="heading">
 
             <span className="heading-1">Pixie MGMT</span>
+            <span className="block uppercase mt-6 text-3xl text-nowrap">about me section goes here</span>
           </h1>
-          <a href="./about.html" className="contact-btn">Read More</a>
+           
 
         </div>
-        <section className="galaxy-wrapper" id="particles">
-          <canvas id="galaxy" width="100%" height="100%"></canvas>
-        </section>
-      </header>
+        <img src="../../pink-circle.png" alt="rose" className="absolute h-auto w-3/12 object-cover top-30 right-0 -z-10" />
+        <img src="../../round.png" alt="rose" className="absolute h-auto w-3/12 object-cover bottom-0 left-0   -z-10" />
 
+      </header>
       <main>
 
-      <section className="py-24">
+        <section className="py-24 relative">
+      
           <div className="flex items-center justify-center">
             <AccordionDemo />
           </div>
-         
+          <img src="../../cherry.png" alt="rose" className="absolute h-auto w-3/12 object-cover bottom-30 right-40 -z-10" />
         </section>
 
 
-      <section className="contact-section" id="contact-section">
-            <div className="contact-title">
-                <h1 className="contact-heading">
-                    <span className="contact">CONTACT</span>
+        <section className="contact-section relative" id="contact-section">
+          <div className="contact-title">
+            <h1 className="contact-heading">
+              <span className="contact text-4xl font-semibold">CONTACT</span>
 
 
-                </h1>
+            </h1>
+          </div>
+
+
+          <form className="contact">
+            <div className="input-group">
+              <input type="text" placeholder="Name *" />
+              <input type="text" placeholder="Email *" />
             </div>
-              
-
-            <form className="contact">
-                <div className="input-group">
-                    <input type="text" placeholder="Name *"/>
-                    <input type="text" placeholder="Email *"/>
-                </div>
-                <div className="input-group">
-                    <input type="text" placeholder="Phone"/>
-                    <input type="text" placeholder="OnlyFans"/>
-                </div>
-                <div className="input-group">
-                    <input type="text" placeholder="Instagram"/>
-                    <input type="text" placeholder="TikTok"/>
-                </div>
-                <div className="input-group">
-                    <input type="text" placeholder="Twitter"/>
-                    <input type="text" placeholder="Message"/>
-                </div>
-                <input type="button" value="Send"/>
-            </form>
+            <div className="input-group">
+              <input type="text" placeholder="Phone" />
+              <input type="text" placeholder="OnlyFans" />
+            </div>
+            <div className="input-group">
+              <input type="text" placeholder="Instagram" />
+              <input type="text" placeholder="TikTok" />
+            </div>
+            <div className="input-group">
+              <input type="text" placeholder="Twitter" />
+              <input type="text" placeholder="Message" />
+            </div>
+            <input type="button" value="Send" />
+          </form>
 
 
         </section>
-
+        <img src="../../3d.png" alt="rose" className="absolute h-auto w-3/12 object-cover bottom-20 left-40 -z-10" />
       </main>
+      <img src="../../3d.png" alt="rose" className="absolute h-auto w-3/12 object-cover   left-40 -z-10" />
+      <footer className="px-12 pt-24 pb-6">
+        <div className="flex items-center justify-between">
+          <img src="../../logo.png" alt="logo" className="logo-img" />
+          <p className="text-white xl:text-2xl text-[1.2rem]">Copyright © 2024 Pixie MGMT</p>
+        <div className="flex flex-col items-center mt-[7rem]">
+          <h5 className="text-rose-100 text-3xl  font-semibold mb-4">Follow On</h5>
+          <a className="text-rose-100 text-2xl" href="">Instagram</a>
+          <a className="text-rose-100 text-2xl my-3" href="">Twitter</a>
+          <a className="text-rose-100 text-2xl" href="mailto:">Email</a>
+        </div>
+        </div>
+      </footer>
 
-      <footer className="footer">
-        <p className="footer-text">Copyright © 2024 Pixie MGMT</p>
-    </footer>
     </>
   );
 }
--- src/components/AccordionDemo.tsx
+++ src/components/AccordionDemo.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import React, { useState } from "react";
-import { Plus, Minus, Snowflake, Sparkle, Triangle } from "lucide-react";
+import { Plus, Minus, Snowflake, Sparkle, Triangle, Flower, Flower2, Fan } from "lucide-react";
 
 export function AccordionDemo() {
     const [openAccordion, setOpenAccordion] = useState(0); // Track the open accordion
@@ -15,7 +15,7 @@ export function AccordionDemo() {
     };
 
     return (
-        <div className="w-[80%]">
+        <div className="w-[90%]">
             <div>
                 {/* Accordion Item 1 */}
                 <div className="border-b">
@@ -24,20 +24,20 @@ export function AccordionDemo() {
                         onClick={() => toggleAccordion(1)}
                     >
                         <div className="flex items-center justify-center gap-3">
-                            <span className="text-white  sm:text-lg text-nowrap">01</span>
-                            <Snowflake className="text-white" />
-                            <span className="text-lg font-medium text-white pb-2">Onlyfans Account Management</span>
+                            <span className="text-white text-3xl text-nowrap">01</span>
+                            <Flower className="text-white" /> 
+                            <span className="text-2xl font-medium text-white pb-2 w-full">Onlyfans Account Management</span>
                         </div>
                         {openAccordion === 1 ? <Minus className="text-white" /> : <Plus className="text-white" />}
                     </button>
                     <div className={`transition-height duration-500 ease-in-out overflow-hidden ${openAccordion === 1 ? 'h-auto' : 'h-0'}`}>
                         <div className="ml-3">
-                            <div className="pb-1 text-sm text-white">You will have an account manager who will take care of you 24/7</div>
-                            <p className="pb-1 text-sm text-white">Onlyfans Chatter</p>
-                            <p className="pb-1 text-sm text-white">Guides to help you with content creation</p>
-                            <p className="pb-1 text-sm text-white">Fan management (refunds, recoveries,…)</p>
-                            <p className=" text-white text-sm pb-1">PPV + tip sales and upsell management</p>
-                            <p className=" text-white text-sm pb-1">General weekly audits and reports for social media and OnlyFans.</p>
+                            <div className="pb-1 text-xl text-white">You will have an account manager who will take care of you 24/7</div>
+                            <p className="pb-1 text-xl text-white">Onlyfans Chatter</p>
+                            <p className="pb-1 text-xl text-white">Guides to help you with content creation</p>
+                            <p className="pb-1 text-xl text-white">Fan management (refunds, recoveries,…)</p>
+                            <p className=" text-white text-xl pb-1">PPV + tip sales and upsell management</p>
+                            <p className=" text-white text-xl pb-1">General weekly audits and reports for social media and OnlyFans.</p>
                         </div>
                     </div>
                 </div>
@@ -48,18 +48,18 @@ export function AccordionDemo() {
                         onClick={() => toggleAccordion(2)}
                     >
                         <div className="flex gap-3">
-                            <span className="text-white text-lg">02</span>
-                            <Sparkle className="text-white" />
-                            <span className="text-lg font-medium text-white pb-2">Onlyfans Content Creation</span>
+                            <span className="text-white text-3xl">02</span>
+                            <Flower2 className="text-white"/>  
+                            <span className="text-2xl font-medium text-white pb-2">Onlyfans Content Creation</span>
                         </div>
                         {openAccordion === 2 ? <Minus className="text-white" /> : <Plus className="text-white" />}
                     </button>
                     <div className={`transition-height duration-500 ease-in-out overflow-hidden ${openAccordion === 2 ? 'h-auto' : 'h-0'}`}>
                         <div className="ml-3">
-                            <div className="pb-1 text-sm text-white">Complete audit of all your account performance, revenue, chats, and your account</div>
-                            <p className="pb-1 text-sm text-white">Audits to better understand where weaknesses lie and where potential exists</p>
-                            <p className="pb-1 text-sm text-white">Precise steps to turn these weaknesses into strengths and significantly increase your revenue with less time spent on them</p>
-                            <p className="pb-1 text-sm text-white">+ Social media audits to get the most out of your brand</p>
+                            <div className="pb-1 text-xl text-white">Complete audit of all your account performance, revenue, chats, and your account</div>
+                            <p className="pb-1 text-xl text-white">Audits to better understand where weaknesses lie and where potential exists</p>
+                            <p className="pb-1 text-xl text-white">Precise steps to turn these weaknesses into strengths and significantly increase your revenue with less time spent on them</p>
+                            <p className="pb-1 text-xl text-white">+ Social media audits to get the most out of your brand</p>
 
                         </div>
                     </div>
@@ -71,18 +71,18 @@ export function AccordionDemo() {
                         onClick={() => toggleAccordion(3)}
                     >
                         <div className="flex gap-3">
-                            <span className="text-white text-lg">03</span>
-                            <Triangle className="text-white" />
-                            <span className="text-lg font-medium text-white pb-2">Onlyfans Marketing</span>
+                            <span className="text-white text-3xl">03</span>
+                            <Fan  className="text-white"/>
+                            <span className="text-2xl font-medium text-white pb-2">Onlyfans Marketing</span>
                         </div>
                         {openAccordion === 3 ? <Minus className="text-white" /> : <Plus className="text-white" />}
                     </button>
                     <div className={`transition-height duration-500 ease-in-out overflow-hidden ${openAccordion === 3 ? 'h-auto' : 'h-0'}`}>
                         <div className="ml-3">
-                            <div className="pb-1 text-sm text-white">Social media management for existing or new accounts (Instagram, Reddit, Twitter and many more).</div>
-                            <p className="pb-1 text-sm text-white">Daily Reddit promotion (with our automations we make sure to post there 24/7).</p>
-                            <p className="pb-1 text-sm text-white">We take care of your direct messages on social media</p>
-                            <p className="pb-1 text-sm text-white">We organize promotions like shoutouts, GGS and much more</p>
+                            <div className="pb-1 text-xl text-white">Social media management for existing or new accounts (Instagram, Reddit, Twitter and many more).</div>
+                            <p className="pb-1 text-xl text-white">Daily Reddit promotion (with our automations we make sure to post there 24/7).</p>
+                            <p className="pb-1 text-xl text-white">We take care of your direct messages on social media</p>
+                            <p className="pb-1 text-xl text-white">We organize promotions like shoutouts, GGS and much more</p>
 
                         </div>
                     </div>
